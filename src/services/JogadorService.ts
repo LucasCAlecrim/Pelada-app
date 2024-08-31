a19fe26c14ccc84220072eaa9212e4424a659d2f
LucasCalecrim <lucascardosoalecrim99@gmail.com>
2024-08-31 10:02:02 -0300

    Novo metodo para gerar buscas

--- src/services/JogadorService.ts
+++ src/services/JogadorService.ts
@@ -5,10 +5,12 @@ import { JogadorDTO } from '../dto/JogadorDTO';
 export class JogadorService {
     private jogadorRepository = AppDataSource.getRepository(Jogador);
 
+    
     async create(jogador: Jogador): Promise<Jogador> {
         return await this.jogadorRepository.save(jogador);
     }
 
+    
     async findAll(): Promise<JogadorDTO[]> {
         const jogadores = await this.jogadorRepository.find();
         return jogadores.map(({ id, apelido, gols, assistencias, posicao }) => ({
@@ -20,19 +22,86 @@ export class JogadorService {
         }));
     }
 
+   
     async findOne(id: number): Promise<Jogador | null> {
         return await this.jogadorRepository.findOneBy({ id });
     }
 
+    
     async delete(id: number): Promise<void> {
         await this.jogadorRepository.delete(id);
     }
 
+    
     async getRanking(): Promise<JogadorDTO[]> {
         const jogadores = await this.jogadorRepository.find({
             order: {
                 gols: 'DESC',
                 assistencias: 'DESC',
+            },
+        });
+        return jogadores.map(({ id, apelido, gols, assistencias, posicao }) => ({
+            id,
+            apelido,
+            gols,
+            assistencias,
+            posicao,
+        }));
+    }
+
+    
+    async listarPorGolsDecrescente(): Promise<JogadorDTO[]> {
+        const jogadores = await this.jogadorRepository.find({
+            order: {
+                gols: 'DESC',
+            },
+        });
+        return jogadores.map(({ id, apelido, gols, assistencias, posicao }) => ({
+            id,
+            apelido,
+            gols,
+            assistencias,
+            posicao,
+        }));
+    }
+
+    
+    async listarPorAssistenciasDecrescente(): Promise<JogadorDTO[]> {
+        const jogadores = await this.jogadorRepository.find({
+            order: {
+                assistencias: 'DESC',
+            },
+        });
+        return jogadores.map(({ id, apelido, gols, assistencias, posicao }) => ({
+            id,
+            apelido,
+            gols,
+            assistencias,
+            posicao,
+        }));
+    }
+
+   
+    async listarPorGolsCrescente(): Promise<JogadorDTO[]> {
+        const jogadores = await this.jogadorRepository.find({
+            order: {
+                gols: 'ASC',
+            },
+        });
+        return jogadores.map(({ id, apelido, gols, assistencias, posicao }) => ({
+            id,
+            apelido,
+            gols,
+            assistencias,
+            posicao,
+        }));
+    }
+
+   
+    async listarPorAssistenciasCrescente(): Promise<JogadorDTO[]> {
+        const jogadores = await this.jogadorRepository.find({
+            order: {
+                assistencias: 'ASC',
             },
         });
         return jogadores.map(({ id, apelido, gols, assistencias, posicao }) => ({
